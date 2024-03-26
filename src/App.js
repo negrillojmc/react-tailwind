a98411b7329e52c9664efcdfc3aa015f48ca87a5
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-03-27 05:30:17 +0800

    Created the Hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
