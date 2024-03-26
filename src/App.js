23829857377ac29dd5807fa6cce0bdc0a7322770
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-03-27 06:14:07 +0800

    Created Analytics section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import Analytics from "./Components/Analytics";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Analytics />
     </div>
   );
 }
