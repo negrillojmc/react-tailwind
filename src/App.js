e26c3cfc6449411d5a0e3c7685e689d8673df86f
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-03-27 22:06:48 +0800

    Created the Newsletter Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Analytics from "./Components/Analytics";
+import Newsletter from "./Components/Newsletter";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }
